d4463d782b4c060c2af4ef81f6c420cfbcdee95c
Luiz Htet <nyanlinhtet.hza@gmail.com>
2025-02-01 21:55:13 +0630

    2nd change

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,11 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            <p>hello World!!</p>
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+              Hello World My Fri "<-_->"
+            </code>
+            .
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
